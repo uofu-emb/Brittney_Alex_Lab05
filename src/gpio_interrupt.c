91432f45072f469f6d51f0fff8217b56fe8bb7d0
CompuGuy99 <alex@compuguy.dev>
2023-10-04 16:23:11 -0600

    Added a message queue to the gpio_interrupt main and tested successfully.

--- src/gpio_interrupt.c
+++ src/gpio_interrupt.c
@@ -1,28 +1,58 @@
 #include <zephyr.h>
-#include <zephyr.h>
 #include <device.h>
+#include <kernel.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+
+typedef struct k_thread k_thread_t;
+typedef struct k_msgq k_msgq_t;
 
 #define DEV_IN DT_NODELABEL(gpioa)
 #define DEV_OUT DT_NODELABEL(gpioa)
 #define PIN_OUT 0
 #define PIN_IN 1
+#define STACKSIZE 1000
 
 struct gpio_callback callback;
 const struct device *dev_in, *dev_out;
 
-K_MSGQ_DEFINE(request, sizeof(struct request_msg), 32, 4);
+k_thread_t toggle_thread;
+K_THREAD_STACK_DEFINE(toggle_stack, STACKSIZE);
+
+K_MSGQ_DEFINE(request, sizeof(int), 32, 4);
+
 
 void pin_interrupt(const struct device *port,
                    struct gpio_callback *cb,
                    gpio_port_pins_t pins_)
 {
-    gpio_pin_toggle(dev_out, PIN_OUT);
+    k_msgq_put(&request, 1, K_FOREVER);
+    // gpio_pin_toggle(dev_out, PIN_OUT);
+}
+
+void message_handler(k_msgq_t *requests)
+{
+    while (1)
+    {
+        int i;
+        k_msgq_get(requests, i, K_FOREVER);
+        gpio_pin_toggle(dev_out, PIN_OUT);
+    }
 }
 
 void interrupt_main(void)
 {
+    k_thread_create(&toggle_thread,
+                    toggle_stack,
+                    STACKSIZE,
+                    (k_thread_entry_t) message_handler,
+                    &request,
+                    NULL,
+                    NULL,
+                    K_PRIO_COOP(7),
+                    0,
+                    K_NO_WAIT);
+    
     dev_in = device_get_binding(DT_LABEL(DEV_IN));
     dev_out = device_get_binding(DT_LABEL(DEV_OUT));
 
@@ -33,3 +63,5 @@ void interrupt_main(void)
     gpio_add_callback(dev_in, &callback);
     k_sleep(K_FOREVER);
 }
+
+
